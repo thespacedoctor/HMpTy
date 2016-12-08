94cb611098f9196cb5efb818e7e8c4cb3610edb1
thespacedoctor <nothingbutdave@gmail.com>
2016-12-08 11:21:27 +0000

    x

--- HMpTy/mysql/add_htm_ids_to_mysql_database_table.py
+++ HMpTy/mysql/add_htm_ids_to_mysql_database_table.py
@@ -30,7 +30,7 @@ def add_htm_ids_to_mysql_database_table(
         log,
         primaryIdColumnName="primaryId",
         cartesian=False,
-        batchSize=50000,
+        batchSize=500000,
         reindex=False):
     """*Given a database connection, a name of a table and the column names for RA and DEC, generates ID for one or more HTM level in the table*
 
